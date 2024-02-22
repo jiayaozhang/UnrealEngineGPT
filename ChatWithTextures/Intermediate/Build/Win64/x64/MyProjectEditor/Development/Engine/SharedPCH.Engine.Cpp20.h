4aebb15cae074459c160fd4714e91ef96f1f579b
jiayaozhang <jiayao_zhang@outlook.com>
2024-02-22 17:55:02 +0800

    UE Usage

---
+++ ChatWithTextures/Intermediate/Build/Win64/x64/MyProjectEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "D:/openai/UnrealEngineGPT/ChatWithTextures/Intermediate/Build/Win64/x64/MyProjectEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ ChatWithTextures/Intermediate/Build/Win64/x64/MyProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "D:/openai/UnrealEngineGPT/ChatWithTextures/Intermediate/Build/Win64/x64/MyProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
